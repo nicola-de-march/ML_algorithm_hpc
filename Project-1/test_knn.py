811bd14b98cfbc6540247fb8877a6b64d6ff4b81
Nicola De March <81023350+nicola-de-march@users.noreply.github.com>
2024-11-21 20:36:39 +0100

    Update test_knn.py

--- Project-1/test_knn.py
+++ Project-1/test_knn.py
@@ -1,21 +1,28 @@
 from KNNClassifier import KNNClassifier
 import numpy as np
 from mpi4py import MPI
+import time
 
 def main():
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
+    start_time = MPI.Wtime()
+
     rows = 100000
     cols = 500
+    
+    # Emaulate a parallel reading of the dataset 
     np.random.seed(699)
     X_train = np.random.rand(rows * cols).reshape((rows, cols))
     y_train = np.random.randint(2, size=rows)
+    # ---------------------------------------------------------
+
     knn = KNNClassifier(k=2)
     knn.fit(X_train, y_train)
 
-    test_size = 100    
+    test_size = 1000    
     assert size <= test_size, "Use less cores to run the program."
     if rank == 0:
         print("---------------------------------------------------------------------")
@@ -32,12 +39,17 @@ def main():
         print(f'X_train shape {X_train.shape} - y_train shape {y_train.shape}')
     predictions = knn.predict(X_test)
     
+    end_time = MPI.Wtime()
+
     if rank == 0:
         #print(f'Predictions: {predictions}')
         #print(f'Label:      {y_train[X_test_indices]}')
         correct = np.sum(y_train[X_test_indices] == predictions)
         print(f'Correct predictions: {correct}/{test_size}')
-
+        print(f"Execution time: {end_time - start_time} s")
+        with open("mpi_knn_results.csv", "a") as file:
+            file.write(f"{size}, {rows}, {test_size}, {end_time - start_time}\n")
+        
 if __name__ == "__main__":
     main()
 
